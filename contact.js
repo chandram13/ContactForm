3cfcd1a9e3b8e92d1dbec2b5825aec14242f1362
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-22 08:14:26 -0800

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,9 @@
+// Marvish Chandra
+
+class contactForm{
+    function formCompletion(name,email,message){
+        fullName = name + "Please give first and last";
+        givenEmail = email + "For faster reply times, use a gmail.";
+        givenMessage = message + "Please keep the message short in length.";
+    }
+}
